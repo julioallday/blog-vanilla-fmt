5ad5cd65ad7fd5d75f0e2e9062f697cad9612e28
julioallday <naruto481@gmail.com>
2023-03-11 16:28:20 -0300

    feat(botaoCarregar): Adiciona botao carregar posts

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,13 @@
-const page = 1;
-const limit = 20;
+const fetchPosts = async (postPage, postLimit = 20) =>
+  await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_page=${postPage}&_limit=${postLimit}`
+  ).then((res) => res.json());
 
-
-const fetchPosts = async (page, limit) => await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`).then((res) => res.json());
-
-const mostraHome = async () => {
+const listarPosts = async () => {
+  let postPage = 1;
   const main = document.getElementById("posts");
-  const posts = await fetchPosts(page, limit)
-  posts.forEach((element) => {
+  const posts = await fetchPosts(postPage);
+  posts.forEach((element, index, array) => {
     const post = document.createElement("div");
     post.setAttribute("class", "blog-post");
     main.appendChild(post);
@@ -16,7 +16,15 @@ const mostraHome = async () => {
         <h4>${element.title}</h4>
         <p>${element.body}</p>
     </a>`;
-    console.log(element);
+    let button = document.getElementById("fetch-posts");
+    if (array.length < 20) {
+      button.classList.remove("show");
+      button.classList.add("hide");
+    } else {
+      button.classList.remove("hide");
+      button.classList.add("show");
+    }
   });
 };
-mostraHome();
+listarPosts();
+
